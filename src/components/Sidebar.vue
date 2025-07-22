3a7b88fc13e54b4ee5a1ffb3cae30248f4afbc31
ankitsinghwakefit <ankitsinghwakefit@gmail.com>
2025-07-22 15:05:42 +0530

    added sidebar and top bar and some structure to dashboard

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-list dense nav>
+        <v-list-item to="/dashboard" router>
+          <v-list-item-content class="d-flex align-center text-h5">
+            <v-list-item-icon class="mr-3">
+              <v-icon>mdi-map</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="text-h6">Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/vehicles" router>
+          <v-list-item-content class="d-flex align-center text-h5">
+            <v-list-item-icon class="mr-3">
+              <v-icon>mdi-truck</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="text-h6">Vehicles</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,28 @@
 <template>
-    <MapView />
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" permanent color="#F3F4F6">
+      <Sidebar />
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#1F2937" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My Fleet Dashboard</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <MapView />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
-import MapView from '@/components/MapView.vue';
-
+import { ref, computed } from "vue";
+import { useDisplay } from "vuetify";
+import MapView from "@/components/MapView.vue";
+import Sidebar from "@/components/Sidebar.vue";
+const drawer = ref(true);
+const { mdAndDown } = useDisplay();
+const isMobile = computed(() => mdAndDown.value);
 </script>
