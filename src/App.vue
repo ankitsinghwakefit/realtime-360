f30d7610b28b14491ca382569b9a445861f0f32d
ankitsinghwakefit <ankitsinghwakefit@gmail.com>
2025-07-22 16:55:15 +0530

    moved sidebar and topbar to global file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-navigation-drawer app v-model="drawer" permanent color="#F3F4F6">
+      <Sidebar />
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#1F2937" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My Fleet Dashboard</v-toolbar-title>
+    </v-app-bar>
     <v-main>
        <router-view />
     </v-main>
@@ -7,4 +15,7 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+import Sidebar from "@/components/Sidebar.vue";
+const drawer = ref(true);
 </script>
